92aa974f74c9e5eb4fc0757173fcbe6663ca066c
CarnunMP <carnun@hotmail.co.uk>
2019-11-15 08:35:12 +0000

    added users-model.js; POST /auth/register done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../api/models/users-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  const userToPost = { ...user, password: hash };
+
+  Users.add(userToPost)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(err => {
+    res.status(500).send('Failed to POST /auth/register: ' + err.message);
+  });
 });
 
 router.post('/login', (req, res) => {
